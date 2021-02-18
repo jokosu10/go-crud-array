b2fca7592d40a394db7ee18703c0110fa795c440
Joko Susilo <susilo.j8@gmail.com>
2021-02-18 17:09:46 +0700

    Get all article and get article by id

--- main.go
+++ main.go
@@ -3,14 +3,15 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/labstack/echo/v4"
 )
 
 type Article struct {
-	Title       string `form:"title"`
-	Description string `form:"description"`
-	Content     string `form:"content"`
+	Title       string `form:"title" json:"title"`
+	Description string `form:"description" json:"description"`
+	Content     string `form:"content" json:"content"`
 }
 
 var articles []Article
@@ -29,6 +30,7 @@ func createaArticle(c echo.Context) error {
 	return c.NoContent(http.StatusCreated)
 }
 
+// method print article in console
 func printArticles(articles []Article) {
 	for i, article := range articles {
 		fmt.Printf("%d. %s\n", i+1, article.Title)
@@ -37,9 +39,29 @@ func printArticles(articles []Article) {
 	fmt.Printf("Total article: %d\n", len(articles))
 }
 
+// method print article
+func showArticle(c echo.Context) error {
+	articleId, err := strconv.Atoi(c.Param("id"))
+
+	if len(articles) < articleId {
+		return c.NoContent(http.StatusNotFound)
+	}
+
+	if err != nil {
+		return c.NoContent(http.StatusInternalServerError)
+	}
+
+	return c.JSON(http.StatusOK, c.JSON(http.StatusOK, articles[articleId]))
+}
+
 func main() {
+	articles = make([]Article, 0)
 	e := echo.New()
 	e.POST("/articles", createaArticle)
+	e.GET("/articles", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, articles)
+	})
+	e.GET("/articles/:id", showArticle)
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
