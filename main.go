1bd1431ce3ef5e42e0aa858eb4015358950711cb
Joko Susilo <susilo.j8@gmail.com>
2021-02-18 15:59:27 +0700

    Create article

--- main.go
+++ main.go
@@ -1,19 +1,45 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
 )
 
 type Article struct {
-	title       string
-	description string
-	content     string
+	Title       string `form:"title"`
+	Description string `form:"description"`
+	Content     string `form:"content"`
+}
+
+var articles []Article
+
+// method create
+func createaArticle(c echo.Context) error {
+	var article Article
+
+	if err := c.Bind(&article); err != nil {
+		return err
+	}
+	articles = append(articles, article)
+
+	printArticles(articles)
+
+	return c.NoContent(http.StatusCreated)
+}
+
+func printArticles(articles []Article) {
+	for i, article := range articles {
+		fmt.Printf("%d. %s\n", i+1, article.Title)
+	}
+
+	fmt.Printf("Total article: %d\n", len(articles))
 }
 
 func main() {
 	e := echo.New()
+	e.POST("/articles", createaArticle)
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
